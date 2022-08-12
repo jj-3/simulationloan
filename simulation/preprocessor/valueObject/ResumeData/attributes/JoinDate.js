bd8760fe5ddbf6dfb6531bf3237e2f986e6e73a2
jj-3 <59683707+jj-3@users.noreply.github.com>
2022-08-12 10:56:46 +0900

    入社年月日のロジック変更（１４歳以下の就業開始は非承認）

--- simulation/preprocessor/valueObject/ResumeData/attributes/JoinDate.js
+++ simulation/preprocessor/valueObject/ResumeData/attributes/JoinDate.js
@@ -1,15 +1,12 @@
 "use strict";
 const { DateType } = require("../../../general/Types")
-
 const format = "YYYYMM"
 const tz = "Asia/Tokyo"
-
 const dayjs = require("dayjs")
 dayjs.extend(require("dayjs/plugin/utc"))
 dayjs.extend(require("dayjs/plugin/timezone"))
 dayjs.extend(require("dayjs/plugin/isBetween"))
 dayjs.tz.setDefault("Asia/Tokyo")
-
 const { ValidationErrors } = require("../../../general/Errors")
 
 /** 入社年月日 */
@@ -20,17 +17,13 @@ class JoinDate extends DateType {
     }
     static of(dateOfCompanyJoin,dateOfPreviousCompanyJoin,birthday, dtToday) {        
         const dtJoinDate = dateOfCompanyJoin.toDate(); //現職の入社年月日
-
         const dtBirthday = birthday.toDate();
-
         const dtAtFifteenth = dtBirthday.add(15, "year")
         var year = dtAtFifteenth.year()
         if (dtAtFifteenth.month() >= 3) {
             year += 1
         }
-
         const dtWorkable = dayjs(new Date(year, 3, 1)) // 満15歳の4/1
-
         if (!dtJoinDate.isBetween(dtWorkable, dtToday, "day", "[]")) {
             var workable = dtWorkable.format(format)
             var today = dtToday.format(format)
@@ -38,13 +31,28 @@ class JoinDate extends DateType {
                 `Value must be in range between '${workable}' and '${today}'. (at calcurated 'joinDate')`
             )
         }
-
         const termOfService = dtToday.diff(dtJoinDate, "year");//現職の勤続年数を算出
         if (!dateOfPreviousCompanyJoin.value){
             return new JoinDate(dtJoinDate.format(format));
         } else {
             const dtPreviousJoinDate = dateOfPreviousCompanyJoin.toDate();
-            const oldTermOfService = dtToday.diff(dtPreviousJoinDate,"year") - termOfService;  
+            const oldTermOfService = dtToday.diff(dtPreviousJoinDate,"year") - termOfService;
+            
+            const age = dtToday.diff(birthday.toDate(), "year");
+            if(age < termOfService){
+                throw new ValidationErrors.RangeError(
+                    `Value must be '${age}' > '${termOfService}'. (at calcurated 'joinDate')`
+                )
+            }else if(age < oldTermOfService + termOfService){
+                throw new ValidationErrors.RangeError(
+                    `Value must be '${age}' > '${oldTermOfService + termOfService}'. (at calcurated 'joinDate')`
+                )
+            }
+            if(age - (oldTermOfService + termOfService) < 15){
+                throw new ValidationErrors.RangeError(
+                    `Value must be '${age}' - '${oldTermOfService}' < 15. (at calcurated 'joinDate')`
+                )
+            }
             // 勤続年数が長い方を採用する
             if(termOfService < oldTermOfService){
                 return new JoinDate(dtPreviousJoinDate.format(format));
